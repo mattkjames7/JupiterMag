ec5f33e0557e86a82b27371f3249e352c2658abc
mattkjames7 <mattkjames7@gmail.com>
2021-06-17 09:33:41 +0100

    added trap for integration

---
+++ JupiterMag/__data/libjupitermag/con2020/trap.cc
@@ -0,0 +1,14 @@
+#include "trap.h"
+
+double trap(int n, double *x, double *y) {
+	
+	double a = 0.0;
+	int i;
+	
+	for (i=0,i<i-1;i++) {
+		a += 0.5*(y[i] + y[i+1])*(x[i+1] - x[i]);
+	}
+	
+	return a
+	
+}
---
+++ JupiterMag/__data/libjupitermag/con2020/trap.h
@@ -0,0 +1,9 @@
+#ifndef __TRAP_H__
+#define __TRAP_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
+
+
+double trap(int n, double *x, double *y);
