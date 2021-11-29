5da829883c45c2f35098b2acac8653d3c64e432b
Matt <mattkjames7@gmail.com>
2021-11-29 14:37:19 +0000

    added O4 coeffs

--- JupiterMag/__data/libjupitermag/internal/EncodeCoefficients.py
+++ JupiterMag/__data/libjupitermag/internal/EncodeCoefficients.py
@@ -56,7 +56,7 @@ def EncodeCoefficients(fnamein,fnameout):
 
 if __name__ == "__main__":
 	coeffs = ['vit4coeffs','vipalcoeffs','isaaccoeffs','jrm09coeffs',
-				'vip4coeffs','o6coeffs']
+				'vip4coeffs','o6coeffs','o4coeffs']
 	for c in coeffs:
 		print('Converting {:s}'.format(c))
 		EncodeCoefficients(c+'.dat',c+'.bin')
