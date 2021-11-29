e90b28ac7ec4fb7c55b8bfd663c9e664747ae74e
Matt <mattkjames7@gmail.com>
2021-11-29 14:41:38 +0000

    added SHA coeffs

--- JupiterMag/__data/libjupitermag/internal/EncodeCoefficients.py
+++ JupiterMag/__data/libjupitermag/internal/EncodeCoefficients.py
@@ -56,7 +56,7 @@ def EncodeCoefficients(fnamein,fnameout):
 
 if __name__ == "__main__":
 	coeffs = ['vit4coeffs','vipalcoeffs','isaaccoeffs','jrm09coeffs',
-				'vip4coeffs','o6coeffs','o4coeffs']
+				'vip4coeffs','o6coeffs','o4coeffs','shacoeffs']
 	for c in coeffs:
 		print('Converting {:s}'.format(c))
 		EncodeCoefficients(c+'.dat',c+'.bin')
