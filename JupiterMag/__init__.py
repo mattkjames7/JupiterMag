71326952dffb8258841192450bb27bad3fa659b6
mattkjames7 <mattkjames7@gmail.com>
2022-01-18 17:15:03 +0000

    added setup.py

--- JupiterMag/__init__.py
+++ JupiterMag/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.0.1'
+
 from . import Globals
 from . import Con2020
 #from . import _CFunctions
---
+++ setup.py
@@ -0,0 +1,56 @@
+import setuptools
+from setuptools.command.install import install
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def getversion():
+	'''
+	read the version string from __init__
+	
+	'''
+	#get the init file path
+	thispath = os.path.abspath(os.path.dirname(__file__))+'/'
+	initfile = thispath + 'JupiterMag/__init__.py'
+	
+	#read the file in
+	f = open(initfile,'r')
+	lines = f.readlines()
+	f.close()
+	
+	#search for the version
+	version = 'unknown'
+	for l in lines:
+		if '__version__' in l:
+			s = l.split('=')
+			version = s[-1].strip().strip('"').strip("'")
+			break
+	return version
+	
+version = getversion()
+
+setuptools.setup(
+    name="JupiterMag",
+    version=version,
+    author="Matthew Knight James",
+    author_email="mattkjames7@gmail.com",
+    description="Some magnetic field models for Jupiter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattkjames7/JupiterMag",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX",
+    ],
+    install_requires=[
+		'numpy',
+		'matplotlib',
+	],
+	include_package_data=True,
+)
+
+
+
