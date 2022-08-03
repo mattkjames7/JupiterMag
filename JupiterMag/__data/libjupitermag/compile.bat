df3577d2de8a57f126dcecddbec0d6d5111b24ad
Matt <mattkjames7@gmail.com>
2022-08-03 11:10:18 +0100

    added missing line to windows batch file, removed debugging symbols

--- JupiterMag/__data/libjupitermag/compile.bat
+++ JupiterMag/__data/libjupitermag/compile.bat
@@ -12,10 +12,10 @@ cd ..
 
 
 
-g++ -fPIC -c -lm -std=c++17 -Wextra -g model.cc -o model.o
-g++ -fPIC -c -lm -std=c++17 -Wextra -g trace.cc -o trace.o
-g++ -fPIC -c -lm -std=c++17 -Wextra -g interptraceclosestpos.cc -o interptraceclosestpos.o
+g++ -fPIC -c -lm -std=c++17 -Wextra model.cc -o model.o
+g++ -fPIC -c -lm -std=c++17 -Wextra trace.cc -o trace.o
+g++ -fPIC -c -lm -std=c++17 -Wextra interptraceclosestpos.cc -o interptraceclosestpos.o
 
 	
 
-g++ -fPIC -lm -std=c++17 -Wextra -g -shared libjupitermag.cc *.o con2020/*.o spline/*.o libinternalfield/libinternalfield/*.o -o libjupitermag.dll
+g++ -fPIC -lm -std=c++17 -Wextra -shared libjupitermag.cc *.o con2020/*.o spline/*.o libinternalfield/libinternalfield/*.o -o libjupitermag.dll
