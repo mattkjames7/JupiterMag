73a16ce10111a5b916055ef769fe534c80ec84c9
matt <mattkjames7@gmail.com>
2026-04-11 21:39:15 +0100

    more tests

--- tests/tracefield/test_tracefield_helpers.py
+++ tests/tracefield/test_tracefield_helpers.py
@@ -42,6 +42,12 @@ def _make_trace_dict():
     }
 
 
+def _make_trace_with_footprints():
+    data = _make_trace_dict()
+    data["FP"] = np.arange(data["n"] * 49, dtype="float64").reshape(data["n"], 49)
+    return jm.TraceField(data)
+
+
 def test_tracefield_init_from_dict_preserves_data():
     data = _make_trace_dict()
     trace = jm.TraceField(data)
@@ -70,3 +76,38 @@ def test_tracefield_tracedict_removes_nan_padding_and_ctypes_fields():
     npt.assert_array_equal(out["halpha"][0, 0], np.array([0.0, 1.0, 2.0]))
     npt.assert_array_equal(out["halpha"][0, 1], np.array([4.0, 5.0, 6.0]))
     npt.assert_array_equal(out["halpha"][1, 0], np.array([8.0, 9.0]))
+
+
+def test_tracefield_unpackfootprints_maps_fp_columns_to_named_fields():
+    trace = _make_trace_with_footprints()
+
+    trace._UnpackFootprints()
+
+    assert trace.ionosphere.xn3.shape == (trace.n,)
+    assert trace.surface.lonn.shape == (trace.n,)
+    assert trace.equator.fllen.shape == (trace.n,)
+
+    npt.assert_array_equal(trace.ionosphere.xn3, trace.FP[:, 0])
+    npt.assert_array_equal(trace.ionosphere.latn, trace.FP[:, 31])
+    npt.assert_array_equal(trace.surface.xn3, trace.FP[:, 12])
+    npt.assert_array_equal(trace.surface.mlats, trace.FP[:, 45])
+    npt.assert_array_equal(trace.equator.x3, trace.FP[:, 24])
+    npt.assert_array_equal(trace.equator.fllen, trace.FP[:, 48])
+
+
+def test_tracefield_gettrace_returns_trimmed_trace_arrays():
+    trace = jm.TraceField(_make_trace_dict())
+
+    x, y, z, bx, by, bz, r, rnorm, s, h = trace.GetTrace(0)
+
+    npt.assert_array_equal(x, np.array([1.0, 2.0, 3.0]))
+    npt.assert_array_equal(y, np.array([11.0, 12.0, 13.0]))
+    npt.assert_array_equal(z, np.array([21.0, 22.0, 23.0]))
+    npt.assert_array_equal(bx, np.array([31.0, 32.0, 33.0]))
+    npt.assert_array_equal(by, np.array([41.0, 42.0, 43.0]))
+    npt.assert_array_equal(bz, np.array([51.0, 52.0, 53.0]))
+    npt.assert_array_equal(r, np.array([71.0, 72.0, 73.0]))
+    npt.assert_array_equal(rnorm, np.array([81.0, 82.0, 83.0]))
+    npt.assert_array_equal(s, np.array([61.0, 62.0, 63.0]))
+    assert h.shape == (trace.nalpha, trace.nstep[0])
+    npt.assert_array_equal(h, np.array([[0.0, 1.0, 2.0], [4.0, 5.0, 6.0]]))
